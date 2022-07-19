22ae04cbbde13cc5d7d26859d87e97a2fd39d640
shafirapramatana <firapramatana03@gmail.com>
2022-07-19 15:23:05 +0700

    first commit

--- app/src/main/java/com/fidilaundry/app/util/base/BaseRecyclerItemViewModel.kt
+++
@@ -1,5 +0,0 @@
-package com.fidilaundry.app.util.base
-
-abstract class BaseRecyclerItemViewModel {
-    abstract val itemViewType: Int
-}
--- app/src/main/java/com/fidilaundry/app/util/base/CastExeption.kt
+++
@@ -1,8 +0,0 @@
-package com.fidilaundry.app.util.base
-
-class CastExeption(clazz: Class<*>) : ClassCastException(String.format(MESSAGE, clazz)) {
-
-    companion object {
-        private val MESSAGE = "Failed to cast object to class '%s'"
-    }
-}
